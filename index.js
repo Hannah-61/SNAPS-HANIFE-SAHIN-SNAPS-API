cf07df974dc6ef6db9598ae6199d2d9cb19c7a96
Hanife Sahin <sahinhani@outlook.com>
2025-02-03 05:06:11 -0500

    Updated API and frontend connection

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import photosRoutes from "./routes/photos.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import tags from "./routes/tags.js";
+import photos from "./routes/photos.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
 app.use(express.json());
 
 
-app.use("/images", express.static("./public/images"));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-app.use("/photos", photosRoutes);
+app.use("/tags", tags);
+app.use("/photos", photos);
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Error! Click on Ctrl + c to stop the server`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- routes/tags.js
+++ routes/tags.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import fs from 'fs';
+import tags from '../Data/tags.json' with {type: "json"};
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const tagsData = fs.readFileSync('./data/tags.json');
-  const tags = JSON.parse(tagsData);
-  res.json(tags);
+router.get("/", function (request, response) {
+    response.json(tags);
 });
 
 export default router;
-
